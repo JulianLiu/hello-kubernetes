4d7fc230a96ce6842fc7dbf53028974835876017
Julian Liu <liu.julian@gmail.com>
2018-12-05 19:13:25 +0800

    Restructure files

--- hello/hello.py
+++ hello/hello.py
@@ -6,30 +6,11 @@ import datetime
 from flask import Flask, jsonify
 app = Flask(__name__)
 
-
-# ----------------------------------------------------------------------------------------------------------------------
-# SUPPLEMENTAL CODE
-# ----------------------------------------------------------------------------------------------------------------------
-
-#@app.route("/quote", methods=["GET"])
-#def hello_with_quote():
-#    import urllib.request
-#    import json
-#
-#    res = urllib.request.urlopen("http://qotm")
-#    data = res.read()
-#
-#    return jsonify(message="Hello from Kubernetes!",
-#                   quote=json.loads(data.decode('utf-8')),
-#                   hostname=os.getenv("HOSTNAME"),
-#                   time=datetime.datetime.now().isoformat())
-
-
 @app.route("/", methods=["GET"])
 def hello():
-    return jsonify(message="Hello from Kubernetes!",
-                   hostname=os.getenv("HOSTNAME"),
-                   time=datetime.datetime.now().isoformat()),
+    return jsonify({"message": "Hello from Kubernetes!",
+                    "hostname": os.getenv("HOSTNAME"),
+                    "time": datetime.datetime.now().isoformat()})
 
 
 @app.route("/health", methods=["GET", "HEAD"])
